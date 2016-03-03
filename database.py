2fd118854b95e4565946b1b5b799052246e5709c
James Gray <james.matthew.gray@gmail.com>
2016-03-03 10:13:01 -0800

    Switch to SQLite (WIP - need to verify approach)

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import os
 import subprocess
 import sys
 
@@ -13,32 +14,13 @@ from config import db
 parser = argparse.ArgumentParser(description='Database helper functions')
 parser.add_argument('action', help='One of: `create`, `drop`')
 
-# TODO: Ditch MySQL and sqitch to a SQLite database
-# It doesn't really make sense to require the (theoretical) user to have
-# MySQL installed (a complicated and large thing to do) in order to use this
-# utility
-engine = create_engine('mysql+pymysql://%s:%s@%s/%s' \
-    % (db['user'], db['pass'], db['host'], db['name']))
+engine = create_engine('sqlite:///%s' % db['path'], echo=True)
 
 def setup_database():
     '''
     Create the database and its tables.
     '''
-    print('Creating database %s...' % db['name'])
-
-    try:
-        args = [
-            'mysql',
-            '-u%s' % db['user'],
-            '-p%s' % db['pass'],
-            '-e', 'CREATE DATABASE IF NOT EXISTS %s;' % db['name'],
-        ]
-        print(args)
-        subprocess.check_call(args)
-    except subprocess.CalledProcessError:
-        raise
-    else:
-        print('Database created successfully.')
+    print('Creating database %s...' % db['path'])
 
     print('Creating tables...')
     try:
@@ -54,17 +36,16 @@ def drop_database():
     WARNING: This deletes the database permanently!
     '''
     print('You are about to delete the "%s" database permanently! Are you sure? [Y/n] ' \
-        % db['name'], end='')
+        % db['path'], end='')
     choice = input().lower()
     if choice == 'y':
-        conn = engine.connect()
         try:
-            conn.execute('DROP DATABASE %s' % db['name'])
+            Base.metadata.drop_all(engine)
+            os.remove(db['path'])
         except sqlalchemy.exc.InternalError:
             raise
         else:
             print('Database deleted successfully.')
-        conn.close()
 
 if __name__ == '__main__':
     args = parser.parse_args(sys.argv[1:])
