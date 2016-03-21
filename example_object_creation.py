e9950e6086a9405862c44ef3cf886dba693c8b6d
James Gray <james.matthew.gray@gmail.com>
2016-03-20 23:10:24 -0700

    Factor out individual examples into functions for sanity

--- example_object_creation.py
+++ example_object_creation.py
@@ -9,7 +9,7 @@ from ptm.models.music import Playlist
 from ptm.models.music import Song
 from ptm.models.base import session
 
-def main():
+def music_example():
     # Create artists
     kanye = Artist(name='Kanye West')
     mozart = Artist(name='Wolfgang Amadeus Mozart')
@@ -63,6 +63,13 @@ def main():
     # Insert a song at number 3 in the playlist
     playlist.insert_song(2, love_lockdown)
 
+    session.commit()
+
+    print "Artists: %s, %s" % (kanye, mozart)
+    print "Each artist's songs: Kanye: %s, Mozart: %s" % (kanye.songs, mozart.songs)
+    print "Playlist songs: %s" % playlist.songs
+
+def activity_example():
     # Create pace objects and add to session
     slow, steady, fast, sprint = Pace(speed='Slow'), Pace(speed='Steady'), \
         Pace(speed='Fast'), Pace(speed='Sprint')
@@ -97,11 +104,45 @@ def main():
 
     session.commit()
 
-    print "Artists: %s, %s" % (kanye, mozart)
-    print "Each artist's songs: Kanye: %s, Mozart: %s" % (kanye.songs, mozart.songs)
-    print "Playlist songs: %s" % playlist.songs
     print "Plan segments: %s" % plan.segments
 
+def main():
+    # Create pace objects and add to session
+    slow, steady, fast, sprint = Pace(speed='Slow'), Pace(speed='Steady'), \
+        Pace(speed='Fast'), Pace(speed='Sprint')
+
+    session.add(slow)
+    session.add(steady)
+    session.add(fast)
+    session.add(sprint)
+
+    # Create an activity plan
+    plan = ActivityPlan(name='test')
+    session.add(plan)
+    session.flush()
+
+    # Add 4 segments to the plan
+    plan.append_segment(pace=steady, length=60)
+    plan.append_segment(pace=steady, length=60)
+    plan.append_segment(pace=slow, length=10)
+    plan.append_segment(pace=sprint, length=60)
+
+    # Whoops - I made a mistake and want to delete a segment!
+    plan.delete_segment(1)
+
+    # Fix the second segment
+    plan.update_segment(position=1, pace=fast, length=60)
+
+    # Add a segment to the beginning
+    plan.insert_segment(position=0, pace=slow, length=60)
+
+    # Clean up orphaned segments
+    Segment.remove_orphans()
+
+    session.commit()
+
+    print "Plan segments: %s" % plan.segments
 
 if __name__ == '__main__':
-    main()
+    music_example()
+    activity_example()
