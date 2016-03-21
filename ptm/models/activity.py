ec2c3c62c0782bee228d699c6ccb7632a2f43c74
James Gray <james.matthew.gray@gmail.com>
2016-03-20 22:27:01 -0700

    Add several helper methods for working with Segments

--- ptm/models/activity.py
+++ ptm/models/activity.py
@@ -7,6 +7,7 @@ from sqlalchemy.types import String
 
 from ptm.models.base import Base
 from ptm.models.base import PtmBase
+from ptm.models.base import session
 
 class Pace(Base, PtmBase):
     """
@@ -39,6 +40,42 @@ class ActivityPlan(Base, PtmBase):
     def __repr__(self):
         return '<ActivityPlan(name="%s")>' % self.name
 
+    def append_segment(self, pace, length):
+        """
+        Append a segment to the list with pace `pace` and length `length`.
+        """
+        self.segments.append(Segment(pace=pace, length=length))
+
+    def insert_segment(self, position, pace, length):
+        """
+        Insert a segment with pace `pace` and length `length` at position
+        `position`.
+        """
+        self.segments.insert(position, Segment(pace=pace, length=length))
+
+    def update_segment(self, position, pace=None, length=None):
+        """
+        Update the segment at position `position` with pace `pace` and/or
+        length `length`.
+        """
+        if not (pace or length):
+            # noop
+            return
+
+        segment = self.segments[position]
+        segment.pace = pace or segment.pace
+        segment.length = length or segment.length
+        self.segments[position] = segment
+
+    def delete_segment(self, position):
+        """
+        Delete the segment at position `position` and ensure the deleted segment
+        is purged from the database.
+        """
+        seg = self.segments.pop(position)
+        session.flush()
+        session.delete(seg)
+
 class Segment(Base, PtmBase):
     """
     Segment object containing both a Pace and a time in seconds, tied to an
@@ -65,3 +102,14 @@ class Segment(Base, PtmBase):
             self.pace.speed,
             self.length,
         )
+
+    @classmethod
+    def remove_orphans(cls):
+        """
+        Remove 'orphaned' segments (i.e. segments with no plan.)
+
+        Ideally this won't need to be used if you only add/remove segments using
+        methods of ActivityPlan, however if for whatever reason you end up with
+        orphans you can use this method.
+        """
+        cls.query.filter(cls.plan_id == None).delete(synchronize_session='fetch')
