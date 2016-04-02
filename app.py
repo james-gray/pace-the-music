36d862460d9441bbdbf04e3efbcc4ac2077f032e
Nick <n_warwick@yahoo.ca>
2016-04-02 13:47:28 -0700

    Made things work again

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from PyQt5 import QtWidgets
-
+import sys
 from ptm.views.base import PaceTheMusic
+import qdarkstyle
 
 # Initialize the class on startup
 def main():
     app = QtWidgets.QApplication(sys.argv) # new instance of QApplication
     form = PaceTheMusic() # set form to represent the application
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     form.show() # display form
     app.exec_() # execute app
 
