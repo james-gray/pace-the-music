ba28b42065b26fca017676c9d9605bc7b16544a9
James Gray <james.matthew.gray@gmail.com>
2016-04-05 15:37:47 -0700

    Decode strings to UTF 8 to make sure they play nice with our app

--- csv2db.py
+++ csv2db.py
@@ -15,43 +15,44 @@ import csv
 # ./database.py drop
 
 def music_example():
-   csvfile = open('song_dict.csv', 'r')
-   csvreader = csv.reader(csvfile, delimiter=',', quotechar = '\"')
-   for song in csvreader:
-	# Create artists
-        artist_name = unicode(song[1])
+    csvfile = open('song_dict.csv', 'r')
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar = '\"')
+    for song in csvreader:
+        # Create artists
+        artist_name = song[1].decode('utf-8')
         art = Artist.query.filter_by(name=artist_name).first()
-	if not art:
-           # Create new artist
-	   art = Artist(name=unicode(song[1]))
-	   # Add artists to the DB session and flush to generate their
-	   # primary key IDs
-	   session.add(art)
-	   session.flush()
+        if not art:
+            # Create new artist
+            art = Artist(name=artist_name)
+            # Add artists to the DB session and flush to generate their
+            # primary key IDs
+            session.add(art)
+            session.flush()
 
-	# Create songs
-	song2add = Song(
-	   filename = "booo",
-	   #filename=unicode(song[4]),
-	   title=str(song[0]),
-	   date_added=datetime.utcnow(),
-	   artist=art, # Assign the artist object directly
-	)
+        # Create songs
+        song2add = Song(
+            #filename = "booo",
+            filename=song[4].decode('utf-8'),
+            title=song[0].decode('utf-8'),
+            date_added=datetime.utcnow(),
+            artist=art, # Assign the artist object directly
+        )
 
-	# Add songs to the DB session and flush to generate their
-	# primary key IDs as well as populating the artist_id field
-	session.add(song2add)
-	session.flush()
+        # Add songs to the DB session and flush to generate their
+        # primary key IDs as well as populating the artist_id field
+        session.add(song2add)
+        session.flush()
 
-	# Create Metadata
-	meta = SongMeta(
-	   duration = int(float(song[3])),
-	   bpm = int(song[2]),
-	   song = song2add
-	)
-	session.add(meta)
-	session.flush() 
-   session.commit()	
+        # Create Metadata
+        meta = SongMeta(
+            duration = int(float(song[3])),
+            bpm = int(song[2]),
+            song = song2add
+        )
+        session.add(meta)
+        session.flush()
+
+    session.commit()
 
 if __name__ == '__main__':
     music_example()
